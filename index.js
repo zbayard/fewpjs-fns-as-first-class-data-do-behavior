9cc1f7936e99eb849b41b049ebbd95b1c987199e
Zach <zbayard@gmail.com>
2021-01-10 21:17:45 -0500

    commit tree bd03604eacb312f8fad8f94642d24fee9137798c

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+
+  if(parseInt(time) < 12) {
+    return "Good Morning"
+  } else if(parseInt(time) > 17){
+    return "Good Evening"
+  } else{
+    return "Good Afternoon"
+  } 
+  
+
+}
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+
+}
+
 /* Write your implementation of displayMessage() */
